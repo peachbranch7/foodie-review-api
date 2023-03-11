0af91d37e8f94b5079a7597bc790e54343ef790d
peachbranch7 <peachbranch.0517@gmail.com>
2023-03-11 23:53:30 +0900

    fix: 🐛 change a schema for post table

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_08_140647) do
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+    t.string "title", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
